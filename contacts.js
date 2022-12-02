bf16b81aa1941676610dcd34366ece70797e582a
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-12-02 14:57:51 +0200

    add try-catch

--- contacts.js
+++ contacts.js
@@ -4,49 +4,69 @@ const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
-const updateContacts = async (contacts) =>
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+async function updateContacts(contacts) {
+  try {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
 //-------Get all contacts------------------------
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
 //-------Get contact by id------------------------
 async function getContactById(contactId) {
-  const id = String(contactId);
-  const contacts = await listContacts();
-  const result = contacts.find((contact) => contact.id === id);
-  return result || null;
+  try {
+    const id = String(contactId);
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === id);
+    return result || null;
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
 //-------Add contact-----------------------------
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  const contactsList = [...contacts, newContact];
-  //contacts.push(newContact);
-  await updateContacts(contactsList);
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    const contactsList = [...contacts, newContact];
+    await updateContacts(contactsList);
+    return newContact;
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
 //-------Delete contact by id------------------------
 async function removeContact(contactId) {
-  const id = String(contactId);
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) {
-    return null;
+  try {
+    const id = String(contactId);
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+    return result;
+  } catch (err) {
+    console.error(err.message);
   }
-  const [result] = contacts.splice(index, 1);
-  await updateContacts(contacts);
-  return result;
 }
 
 module.exports = {
